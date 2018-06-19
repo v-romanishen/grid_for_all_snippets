ccb65c7f511b402cfae0b00b691bdee0c7f35395
Romanishen Vasya <v.romanishen@example.com>
2018-06-19 13:58:53 +0300

    Only my snippet

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Grid For All Snippets",
+    "summary": """Grid For All Snippets""",
+    "description": """ 
+        Grid For All Snippets
+    """,
+    "version": "0.1",
+    "category": "Website",
+    "depends": [
+        "website",
+        "muk_website_snippet_grid",
+    ],
+    "data": [
+        "snippets/grid.xml",
+        "snippets/snippets.xml",
+    ],
+    "application": False,
+    "installable": True,
+    
+}
